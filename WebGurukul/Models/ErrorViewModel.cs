42c85e0b38d397ac2b60d1aa16bd667c0c8df238
asus <asus@LAPTOP-I7C7DLNC>
2024-08-06 11:07:12 +0530

    Add project files.

---
+++ WebGurukul/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebGurukul.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
